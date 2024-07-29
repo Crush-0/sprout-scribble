3bacb1bcc37055409711d2db7ba59b92bad0a85f
Crush-0 <111955144+Crush-0@users.noreply.github.com>
2024-07-29 23:37:38 +0800

    [init]

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+export default defineConfig({
+  schema: "./server/schema.ts",
+  out: "./server/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRE_URL!,
+  },
+});
